c46244db8f31017326ea8d1521b2371514237011
Morten Nilsen <morten@runsafe.no>
2013-12-15 12:15:21 +0100

    Declare features used

--- src/no/runsafe/combatcooldown/Plugin.java
+++ src/no/runsafe/combatcooldown/Plugin.java
@@ -1,12 +1,14 @@
 package no.runsafe.combatcooldown;
 
 import no.runsafe.framework.RunsafeConfigurablePlugin;
+import no.runsafe.framework.features.Events;
 
 public class Plugin extends RunsafeConfigurablePlugin
 {
 	@Override
 	protected void PluginSetup()
 	{
+		this.addComponent(Events.class);
 		this.addComponent(CombatMonitor.class);
 		this.addComponent(EntityListener.class);
 		this.addComponent(PlayerListener.class);
