048f3b0c8615497781214c5719757813a5c48870
Kruithne <kruithne@gmail.com>
2014-02-12 09:41:10 +0000

    Kill players who log out while in PvP combat (which a shiny effect).

--- src/no/runsafe/combatcooldown/PlayerListener.java
+++ src/no/runsafe/combatcooldown/PlayerListener.java
@@ -1,18 +1,26 @@
 package no.runsafe.combatcooldown;
 
+import no.runsafe.framework.api.ILocation;
+import no.runsafe.framework.api.IWorldEffect;
 import no.runsafe.framework.api.event.player.IPlayerCommandPreprocessEvent;
 import no.runsafe.framework.api.event.player.IPlayerDeathEvent;
+import no.runsafe.framework.api.event.player.IPlayerQuitEvent;
 import no.runsafe.framework.api.log.IDebug;
 import no.runsafe.framework.api.player.IPlayer;
+import no.runsafe.framework.minecraft.Item;
+import no.runsafe.framework.minecraft.WorldBlockEffect;
+import no.runsafe.framework.minecraft.WorldBlockEffectType;
 import no.runsafe.framework.minecraft.event.player.RunsafePlayerCommandPreprocessEvent;
 import no.runsafe.framework.minecraft.event.player.RunsafePlayerDeathEvent;
+import no.runsafe.framework.minecraft.event.player.RunsafePlayerQuitEvent;
 
-public class PlayerListener implements IPlayerCommandPreprocessEvent, IPlayerDeathEvent
+public class PlayerListener implements IPlayerCommandPreprocessEvent, IPlayerDeathEvent, IPlayerQuitEvent
 {
 	public PlayerListener(CombatMonitor combatMonitor, IDebug console)
 	{
 		this.combatMonitor = combatMonitor;
 		this.debugger = console;
+		effect = new WorldBlockEffect(WorldBlockEffectType.BLOCK_DUST, Item.BuildingBlock.Bedrock);
 	}
 
 	@Override
@@ -29,6 +37,19 @@ public class PlayerListener implements IPlayerCommandPreprocessEvent, IPlayerDea
 	}
 
 	@Override
+	public void OnPlayerQuit(RunsafePlayerQuitEvent event)
+	{
+		IPlayer player = event.getPlayer();
+		if (combatMonitor.isInCombat(player.getName()))
+		{
+			player.damage(500D); // This should kill them
+			ILocation location = player.getLocation();
+			if (location != null)
+				location.playEffect(effect, 0.3F, 100, 50);
+		}
+	}
+
+	@Override
 	public void OnPlayerDeathEvent(RunsafePlayerDeathEvent event)
 	{
 		combatMonitor.leaveCombat(event.getEntity());
@@ -36,4 +57,5 @@ public class PlayerListener implements IPlayerCommandPreprocessEvent, IPlayerDea
 
 	private final CombatMonitor combatMonitor;
 	private final IDebug debugger;
+	private final IWorldEffect effect;
 }
